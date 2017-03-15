d7638fc345ad415165c974128c19e66fb6207592
Ravi Ojha <rbojha008@gmail.com>
2017-03-15 23:01:43 +0530

    Initial starter kit with gulp tsc

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require("gulp"),
+	del = require("del"),
+	connect = require("gulp-connect"),
+	shell = require("gulp-shell")
+	rename = require("gulp-rename");
+
+gulp.task("connect", function() {
+	connect.server({
+		name: "Dev App",
+		port: 8080,
+		root: ["dist/"]
+	});
+});
+
+gulp.task("clean", function() {
+	return del(["dist"]);
+})
+
+gulp.task("tsc", ["clean"], shell.task([
+	"tsc"
+], {cwd: "app"}));
+
+gulp.task("tsc_watch", ["clean"], shell.task([
+	"tsc -w"
+], {cwd: "app"}));
+
+gulp.task("html", function() {
+	return gulp.src("app/app.html")
+		.pipe(rename("index.html"))
+		.pipe(gulp.dest("dist"))
+		.pipe(connect.reload());
+});
+
+gulp.task("css", function() {
+	return gulp.src("app/style/**/*.*(css|less)")
+		.pipe(rename("output.css"))
+		.pipe(gulp.dest("dist/css"))
+})
+
+gulp.task("default", ["tsc_watch", "html", "css", "connect"]);
