00916e8361f823924e340eb9385303cb6b016983
Ravi Ojha <rbojha008@gmail.com>
2017-03-16 22:19:11 +0530

    Adding some in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require("gulp"),
 	del = require("del"),
 	connect = require("gulp-connect"),
 	shell = require("gulp-shell")
-	rename = require("gulp-rename");
+	rename = require("gulp-rename"),
+	watch = require("gulp-watch"),
+	sym = require("gulp-sym"),
+	runSequence = require("run-sequence");
 
 gulp.task("connect", function() {
 	connect.server({
@@ -16,16 +19,24 @@ gulp.task("clean", function() {
 	return del(["dist"]);
 });
 
-gulp.task("tsc", ["clean"], shell.task([
+gulp.task("tsc", shell.task([
 	"tsc"
 ], {cwd: "app"}));
 
-gulp.task("tsc_watch", ["clean"], shell.task([
+gulp.task("tsc_watch", shell.task([
 	"tsc -w"
 ], {cwd: "app"}));
 
 gulp.task("html", function() {
 	return gulp.src("app/app.html")
+		.pipe(rename("index.html"))
+		.pipe(gulp.dest("dist"))
+		.pipe(connect.reload());
+});
+
+gulp.task("html_watch", function() {
+	return gulp.src("app/app.html")
+		.pipe(watch("app/app.html"))
 		.pipe(rename("index.html"))
 		.pipe(gulp.dest("dist"))
 		.pipe(connect.reload());
@@ -37,4 +48,44 @@ gulp.task("css", function() {
 		.pipe(gulp.dest("dist/css"))
 });
 
-gulp.task("default", ["tsc_watch", "html", "css", "connect"]);
+gulp.task("css_watch", function() {
+	return gulp.src("app/style/**/*.*(css|less)")
+		.pipe(watch("app/style/**/*.*(css|less)"))
+		.pipe(rename("output.css"))
+		.pipe(gulp.dest("dist/css"))
+});
+
+gulp.task("html", function() {
+	return gulp.src("app/)")
+		.pipe(rename("output.css"))
+		.pipe(gulp.dest("dist/css"))
+});
+
+gulp.task("copy:config", function() {
+	return gulp.src("config.js")
+		.pipe(sym("dist/config.js", {force: true}));
+});
+
+gulp.task("copy:jspm_packages", function() {
+	return gulp.src("jspm_packages")
+		.pipe(sym("dist/jspm_packages", {force: true}));
+});
+
+gulp.task("default", function(cb) {
+	runSequence("clean",
+		[
+			"tsc",
+			"html",
+			"css",
+			"copy:config",
+			"copy:jspm_packages"
+		],
+		"connect",
+		[
+			"tsc_watch",
+			"html_watch",
+			"css_watch",
+		],
+		cb
+	);
+});
